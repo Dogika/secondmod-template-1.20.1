74e062ed1d097c0c6e504112d11cea784fb02b4d
BuildTools <unconfigured@null.spigotmc.org>
2024-12-26 01:00:47 -0500

    Initial commit

---
+++ src/main/java/net/dogika/secondmod/SecondModClient.java
@@ -0,0 +1,10 @@
+package net.dogika.secondmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SecondModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/dogika/secondmod/SecondModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.dogika.secondmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SecondModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
